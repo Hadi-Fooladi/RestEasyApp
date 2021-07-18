7cb19d623effa9a8d0cde93f39a82a4750dd06e8
Mariam Alzaabi <mariam.mk.alzaabi@gmail.com>
2021-07-18 18:34:08 +0400

    Creating Timer

--- src/RestEasyApp/Device/DeviceEmulator/Windows/MainWindow.xaml.cs
+++ src/RestEasyApp/Device/DeviceEmulator/Windows/MainWindow.xaml.cs
@@ -3,6 +3,8 @@ using System.Net;
 using System.Windows;
 using System.Net.Sockets;
 using EasyTCP;
+using System.Windows.Threading;
+using System.ComponentModel;
 
 namespace DeviceEmulator
 {
@@ -12,13 +14,44 @@ namespace DeviceEmulator
 
 		private DataStream Stream;
 
-        public MainWindow()
+		private DispatcherTimer timer = new DispatcherTimer();
+
+		public MainWindow()
 		{
 			InitializeComponent();
 
 			Listener.Start();
 
 			Loop();
+		}
+
+		public void OnWindowClosing(object sender, CancelEventArgs e)
+		{
+			// Stop the timer
+			if (timer.IsEnabled)
+			{
+				timer.Stop();
+				MessageBox.Show("Stopping Timer");
+			}
+		}
+
+		private void timer_Tick(object sender, EventArgs e)
+        {
+			try
+			{
+				var data = new Data
+				{
+					HR = float.Parse(tbHR.Text),
+					RR = float.Parse(tbRR.Text),
+					SPO2 = float.Parse(tbSPO2.Text)
+				};
+
+				Stream?.Send(data);
+			}
+			catch (Exception ex)
+			{
+				MessageBox.Show(ex.Message);
+			}
 		}
 
 		private async void Loop()
@@ -28,6 +61,15 @@ namespace DeviceEmulator
 				var client = await Listener.AcceptTcpClientAsync();
 
 				MessageBox.Show("Connected");
+
+				// Setting a timer for 1 minute
+				timer.Interval = TimeSpan.FromMinutes(1);
+				timer.Tick += timer_Tick;
+
+				// Start the timer
+				timer.Start();
+
+				MessageBox.Show("Starting Timer");
 
 				Stream = new DataStream();
 				Stream.Connect(client);
